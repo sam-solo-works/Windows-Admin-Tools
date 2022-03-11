97381eed85a523e9c2768777d7f1cdb5b1844c26
sam-solo-works <57299464+sam-solo-works@users.noreply.github.com>
2022-03-11 16:11:36 -0500

    Add files via upload

---
+++ getfilepermissions.ps1
@@ -0,0 +1,14 @@
+﻿$filelocationscsv = "C:\Documents\test.csv"
+$filelocationcsvcontents = Get-Content -LiteralPath $filelocationscsv
+$outputfilelocation = "C:\Documents\allfilepermissions.csv"
+
+$results = foreach ($location in $filelocationcsvcontents){
+    if (Test-Path $location){
+    Get-Item -LiteralPath $location -Force | % {Get-acl -path $_.fullname | Select-Object Owner,AccessToString -ExcludeProperty Mode,LastWriteTime,Length,Name -ExpandProperty AccessToString }
+    }
+    else{
+    Write-Output "$location permissions could not be accessed." 
+    }
+} 
+
+$results | Export-CSV $outputfilelocation
---
+++ getfilepermissions_Ver3_0.ps1
@@ -0,0 +1,34 @@
+﻿$filelocationscsv = "C:\Documents\File Share migration_Mednax to NAPA.csv"
+$filelocationcsvcontents = Get-Content -LiteralPath $filelocationscsv
+$usercheck = Get-ADUser -Filter * | where Name -Like $_.split('\')[2,-1]
+
+$AllFolders = foreach ($location in $filelocationcsvcontents) {
+    Get-Item -Path $location -ErrorAction SilentlyContinue
+}
+
+$outputfilelocation = "C:\Documents\allfilepermissions $(get-date -f dd-MMM_hh_mm_ss).csv"
+
+$Results = @()
+Foreach ($_ in $AllFolders) {
+    if (Test-Path $_){
+    Write-Host "$_ working"
+    $Acl = Get-Acl $_ -ErrorAction SilentlyContinue -Filter *
+    foreach ($Access in $acl.Access) {
+    if ($Access.IdentityReference -notlike "BUILTIN\Administrators" -and $Access.IdentityReference -notlike "test\Admins" -and $Access.IdentityReference -notlike "CREATOR OWNER" -and $access.IdentityReference -notlike "NT AUTHORITY\SYSTEM" -and $access.IdentityReference -notlike "Everyone" -and $access.IdentityReference -notlike "BUILTIN\Users" -and $access.IdentityReference -notlike "s-1*") {
+    [string]$perm = $Access.IdentityReference
+    [string]$domain, [string]$groupname = $perm.split('\')
+      if ($checkgroup = Get-ADGroup $groupname){
+            $Properties = [ordered]@{'FolderName'=$_.FullName;'AD Group'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+            $Results += New-Object -TypeName PSObject -Property $Properties
+                }
+            }
+
+        }
+    }
+    else {
+    Write-Host "$_ is not accessible"
+    }
+}
+
+
+$Results | Export-Csv $outputfilelocation -NoTypeInformation
